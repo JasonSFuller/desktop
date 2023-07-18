e64ef341d8a2f20ad51a542e671cdf08f3e469b5
Jason Fuller <JasonSFuller@gmail.com>
2023-07-18 12:18:23 -0400

    add note, update perms

--- win11/vscode/restore-extension-list.ps1
+++ win11/vscode/restore-extension-list.ps1
@@ -1,5 +1,9 @@
 # Usage:
 #   powershell.exe -ExecutionPolicy Bypass -File restore-extension-list.ps1
+# Note:
+#   You may need to TEMPORARILY set the following line in your settings.json,
+#   if you're behind a corporate firewall that performs deep packet inspection.
+#     "http.proxyStrictSSL": false
 
 foreach($ext in Get-Content extensions.txt) {
   Write-Host("Installing extension: $ext")
