fbd0154e4cc8e3dd8ca60ecc2edf2234744540d5
Jason Fuller <JasonSFuller@gmail.com>
2023-11-24 13:56:48 -0500

    disable screensaver tweaks

--- ubuntu-22.04/disable-gnome3-screensaver-global.sh
+++ ubuntu-22.04/disable-gnome3-screensaver-global.sh
@@ -2,22 +2,21 @@
 
 # PURPOSE:  For VMs in a homelab, it usually doesn't make sense to have a
 #   screensaver or the screen locking (annoying), so disable them.  This change
-#   effects all users after they log out/in.  To modify this per user:
+#   effects all users after they log out/in.  
 #
-#     # IMPORTANT:  must be logged in as the user from GUI (so you have context)
-#     gsettings set org.gnome.desktop.session idle-delay 0
-#     gsettings set org.gnome.desktop.screensaver lock-enabled false
-#     gsettings set org.gnome.desktop.screensaver lock-delay 0
+# Adapted from:
+#   https://help.gnome.org/admin/system-admin-guide/stable/desktop-lockscreen.html.en
 
 if [[ "$UID" != "0" ]]; then echo "must be run as root" >&2; exit 1; fi
 
 install -m 0755 -d /etc/dconf/profile
+install -m 0755 -d /etc/dconf/db/local.d/
+
 cat <<- 'EOF' >    /etc/dconf/profile/user
 	user-db:user
 	system-db:local
 	EOF
 
-install -m 0755 -d /etc/dconf/db/local.d/
 cat <<- 'EOF' >    /etc/dconf/db/local.d/00-screensaver
 	[org/gnome/desktop/session]
 	idle-delay=uint32 0
@@ -25,4 +24,5 @@ cat <<- 'EOF' >    /etc/dconf/db/local.d/00-screensaver
     lock-enabled=false
 	lock-delay=uint32 0
 	EOF
+
 dconf update
---
+++ ubuntu-22.04/disable-gnome3-screensaver-user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# PURPOSE:  For VMs in a homelab, it usually doesn't make sense to have a
+#   screensaver or the screen locking (annoying), so disable them.  This change
+#   effects only the currently logged in user (w/ a Gnome session) and will take
+#   effect after they've logged out/in again:
+#
+# IMPORTANT:  User MUST be logged from GUI (so you have context).
+
+if ! grep -q ^GNOME <(env); then echo "ERROR: GNOME session not found" >&2; exit 1; fi
+
+gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.screensaver lock-delay 0
