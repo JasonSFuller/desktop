3e9e4b7fa5a26d058e8d4b39d64124f52d553d6c
Jason Fuller <JasonSFuller@gmail.com>
2023-12-14 21:52:35 -0500

    update vscode settings

--- win11/vscode/backup-extension-list.ps1
+++ win11/vscode/backup-extension-list.ps1
@@ -1,4 +1,4 @@
 # Usage:
 #   powershell.exe -ExecutionPolicy Bypass -File backup-extension-list.ps1
 
-code --list-extensions --show-versions > extensions.txt
+code --list-extensions > extensions.txt
