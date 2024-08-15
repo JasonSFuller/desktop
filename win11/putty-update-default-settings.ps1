d1b0af2635f0b80466bb9dcc192e32795d8b1c2e
Jason Fuller <JasonSFuller@gmail.com>
2024-08-15 11:53:43 -0400

    add comments to color lines

--- win11/putty-update-default-settings.ps1
+++ win11/putty-update-default-settings.ps1
@@ -21,28 +21,28 @@ $values = @{
   # color settings (gnome's tango theme)
   "Xterm256Colour" = 0x00000001
   "BoldAsColour" = 0x00000001
-  "Colour0" = "187,187,187"
-  "Colour1" = "255,255,255"
-  "Colour2" = "8,8,8"
-  "Colour3" = "85,85,85"
-  "Colour4" = "0,0,0"
-  "Colour5" = "0,255,0"
-  "Colour6" = "46,52,54"
-  "Colour7" = "85,87,83"
-  "Colour8" = "204,0,0"
-  "Colour9" = "239,41,41"
-  "Colour10" = "78,154,6"
-  "Colour11" = "138,226,52"
-  "Colour12" = "196,160,0"
-  "Colour13" = "252,233,79"
-  "Colour14" = "52,101,164"
-  "Colour15" = "114,159,207"
-  "Colour16" = "117,80,123"
-  "Colour17" = "173,127,168"
-  "Colour18" = "6,152,154"
-  "Colour19" = "52,226,226"
-  "Colour20" = "211,215,207"
-  "Colour21" = "238,238,236"
+  "Colour0" = "187,187,187"  # Default Foreground
+  "Colour1" = "255,255,255"  # Default Bold Foreground
+  "Colour2" = "8,8,8"        # Default Background
+  "Colour3" = "85,85,85"     # Default Bold Background
+  "Colour4" = "0,0,0"        # Cursor Text
+  "Colour5" = "0,255,0"      # Cursor Colour
+  "Colour6" = "46,52,54"     # ANSI Black
+  "Colour7" = "85,87,83"     # ANSI Black Bold
+  "Colour8" = "204,0,0"      # ANSI Red
+  "Colour9" = "239,41,41"    # ANSI Red Bold
+  "Colour10" = "78,154,6"    # ANSI Green
+  "Colour11" = "138,226,52"  # ANSI Green Bold
+  "Colour12" = "196,160,0"   # ANSI Yellow
+  "Colour13" = "252,233,79"  # ANSI Yellow Bold
+  "Colour14" = "52,101,164"  # ANSI Blue
+  "Colour15" = "114,159,207" # ANSI Blue Bold
+  "Colour16" = "117,80,123"  # ANSI Magenta
+  "Colour17" = "173,127,168" # ANSI Magenta Bold
+  "Colour18" = "6,152,154"   # ANSI Cyan
+  "Colour19" = "52,226,226"  # ANSI Cyan Bold
+  "Colour20" = "211,215,207" # ANSI White
+  "Colour21" = "238,238,236" # ANSI White Bold
 
   # appearance
   "Font" = "Source Code Pro"
@@ -81,8 +81,4 @@ $values = @{
 $values.Keys | %{
   set-itemproperty $sessionKey $_ $values[$_]
 }
-
-
-
-
 
